aa0faa1b8452347cacc3b52f4970cc90c8d35849
Gebruiker <Gebruiker@10.115.191.243>
2017-06-20 09:54:04 +0200

    Edited user portal

--- resources/views/up/index.blade.php
+++ resources/views/up/index.blade.php
@@ -2,13 +2,56 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+    <div class="row up-panel-main">
+        <div class="col-md-8 up-panel">
             <div class="panel panel-default">
                 <div class="jumbotron"><h1>{{ $welcomeMsg }}</h1></div>
 
-                <div class="panel-body">
-                    You are logged in!
+                <div class="panel-body up-panel-body">
+                    <div class="row">
+                        <div class="col-md-4 up-panel-box up-panel-box1">
+                            <div class="row">
+                                <div class="col-lg-2"></div>
+                                <div class="col-lg-8">
+                                    <div class="card" style="width: 20rem;">
+                                        <h1 class="card-button"><span class="glyphicon glyphicon-book" aria-hidden="true"></span></h1>
+                                        <div class="card-block">
+                                            <h2 class="card-title">Card title</h2>
+                                        </div>
+                                    </div>   
+                                </div>
+                                <div class="col-lg-2"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 up-panel-box up-panel-box1">
+                            <div class="row">
+                                <div class="col-lg-2"></div>
+                                <div class="col-lg-8">
+                                    <div class="card" style="width: 20rem;">
+                                        <h1 class="card-button"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span></h1>
+                                        <div class="card-block">
+                                            <h2 class="card-title">Card title</h2>
+                                        </div>
+                                    </div>   
+                                </div>
+                                <div class="col-lg-2"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 up-panel-box up-panel-box1">
+                            <div class="row">
+                                <div class="col-lg-2"></div>
+                                <div class="col-lg-8">
+                                    <div class="card" style="width: 20rem;">
+                                        <h1 class="card-button"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></h1>
+                                        <div class="card-block">
+                                            <h2 class="card-title">Go2Shop</h2>
+                                        </div>
+                                    </div>   
+                                </div>
+                                <div class="col-lg-2"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
